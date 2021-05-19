f6ab698a5e999619db63e22d3452cbc4939e9594
Fabrizio Ervini <faber.rvn@gmail.com>
2021-05-19 18:23:45 +0200

    feat: get sorted post by date on fetch

--- packages/api/src/controllers/postController.js
+++ packages/api/src/controllers/postController.js
@@ -14,7 +14,7 @@ const createPost = async (req, res) => {
 
 const fetchPosts = async (req, res) => {
   try {
-    const posts = await Post.find();
+    const posts = await Post.find().sort({ createdAt: -1 });
     res.status(200).json(posts);
   } catch (error) {
     res.status(404).json({ message: error.message });
